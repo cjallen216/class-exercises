54403bc9b0f9d21eae12190f81c087df3be51286
Chris Allen <christopherjallen216@gmail.com>
2020-12-04 17:20:05 -0500

    Completed Day 4 exercises

--- module-1/04_Loops_Arrays/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -20,22 +20,29 @@ public class Exercises {
 	}
 
 	/*
-	 2. Given an array of ints, return true if the array is length 1 or more, and the first element and
-	 the last element are equal.
+	 2. Given an array of ints, return true if the array is length 1 or more,
+	 and the first element and the last element are equal.
 	 sameFirstLast([1, 2, 3]) → false
 	 sameFirstLast([1, 2, 3, 1]) → true
 	 sameFirstLast([1, 2, 1]) → true
 	 */
-	public boolean sameFirstLast(int[] nums) {
-		return false;
+	public boolean sameFirstLast(int[] nums)
+	{
+		boolean arrayLengthAtLeastOne = nums.length > 0;
+		boolean doesFirstEqualLast = nums[0] == nums[nums.length -1];
+		
+		return arrayLengthAtLeastOne && doesFirstEqualLast;
 	}
 
 	/*
 	 3. Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
 	 makePi() → [3, 1, 4]
 	 */
-	public int[] makePi() {
-		return new int[] {};
+	public int[] makePi()
+	{
+		int[] piArray = new int[] {3, 1, 4};						
+		
+		return piArray;
 	}
 
 	/*
@@ -48,9 +55,12 @@ public class Exercises {
 	 */
 	public boolean commonEnd(int[] a, int[] b)
 	{
-
+		boolean isFirstElementSame = a[0] == b[0];
+		boolean isLastElementSameElementSame = a[a.length - 1] == b[b.length - 1];
 		
-		return false;
+		boolean hereIsTheResult = isFirstElementSame || isLastElementSameElementSame;
+				
+		return hereIsTheResult;
 	}
 
 	/*
@@ -59,8 +69,11 @@ public class Exercises {
 	 sum3([5, 11, 2]) → 18
 	 sum3([7, 0, 0]) → 7
 	 */
-	public int sum3(int[] nums) {
-		return 0;
+	public int sum3(int[] nums)
+	{
+		int sum3 = nums[0] + nums[1] + nums [2];
+		
+		return sum3;
 	}
 
 	/*
@@ -70,86 +83,169 @@ public class Exercises {
 	 rotateLeft3([5, 11, 9]) → [11, 9, 5]
 	 rotateLeft3([7, 0, 0]) → [0, 0, 7]
 	 */
-	public int[] rotateLeft3(int[] nums) {
-		return new int[] {};
+	public int[] rotateLeft3(int[] nums)
+	{
+		int[] newNums = new int[3];
+		newNums[0] = nums[1];
+		newNums[1] = nums[2];
+		newNums[2] = nums[0];
+		
+		return newNums;
 	}
 
 	/*
-	 7. Given an array of ints length 3, return a new array with the elements in reverse order, so 
-	 {1, 2, 3} becomes {3, 2, 1}.
+	 7. Given an array of ints length 3, return a new array with the elements in
+	 reverse order, so  {1, 2, 3} becomes {3, 2, 1}.
 	 reverse3([1, 2, 3]) → [3, 2, 1]
 	 reverse3([5, 11, 9]) → [9, 11, 5]
 	 reverse3([7, 0, 0]) → [0, 0, 7]
 	 */
-	public int[] reverse3(int[] nums) {
-		return new int[] {};
+	public int[] reverse3(int[] nums)
+	{
+		int[] newNums = new int[3];
+		newNums[0] = nums[2];
+		newNums[2] = nums[0];
+		newNums[1] = nums[1];
+		
+		return newNums;
 	}
 
 	/*
-	 8. Given an array of ints length 3, figure out which is larger between the first and last elements 
-	 in the array, and set all the other elements to be that value. Return the changed array.
+	 8. Given an array of ints length 3, figure out which is larger between the first
+	 and last elements 
+	 in the array, and set all the other elements to be that value. Return the
+	 changed array.
+	 
 	 maxEnd3([1, 2, 3]) → [3, 3, 3]
 	 maxEnd3([11, 5, 9]) → [11, 11, 11]
 	 maxEnd3([2, 11, 3]) → [3, 3, 3]
 	 */
-	public int[] maxEnd3(int[] nums) {
-		return new int[] {};
+	public int[] maxEnd3(int[] nums)
+	{
+		int first = nums[0];
+		int last = nums[2];
+		int biggest = first;
+		if (last > first)
+		{
+			biggest = last;
+		}
+		
+		int[] newNums = new int [3];
+		
+			newNums[0] = biggest;
+			newNums[1] = biggest;
+			newNums[2] = biggest;
+		 		
+		return newNums;
 	}
 
 	/*
-	 9. Given an array of ints, return the sum of the first 2 elements in the array. If the array length
-	  is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+	 9. Given an array of ints, return the sum of the first 2 elements in the array.
+	 If the array length is less than 2, just sum up the elements that exist,
+	 returning 0 if the array is length 0.
+	 
 	 sum2([1, 2, 3]) → 3
 	 sum2([1, 1]) → 2
 	 sum2([1, 1, 1, 1]) → 2
 	 */
-	public int sum2(int[] nums) {
-		return 0;
+	public int sum2(int[] nums)
+	{
+		if (nums.length == 0)
+		{
+			return 0;
+		}
+		else if (nums.length < 2)
+		{
+			return nums[0];
+		}
+		
+		return nums[0] + nums[1];
 	}
 
 	/*
-	 10. Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle 
+	 10. Given 2 int arrays, a and b, each length 3,
+	 return a new array length 2 containing their middle 
 	 elements.
 	 middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
 	 middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
 	 middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
 	 */
-	public int[] middleWay(int[] a, int[] b) {
-		return new int[] {};
+	public int[] middleWay(int[] a, int[] b)
+	{
+		int[] middleNumbers = new int[2];
+		
+		middleNumbers[0] = a[1];
+		middleNumbers[1] = b[1];
+		
+		return middleNumbers;
 	}
 
 	/*
-	 11. Return the number of even ints in the given array. Note: the % "mod" operator computes the 
-	 remainder, e.g. 5 % 2 is 1.
+	 11. Return the number of even ints in the given array.
+	 Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+	 
 	 countEvens([2, 1, 2, 3, 4]) → 3
 	 countEvens([2, 2, 0]) → 3
 	 countEvens([1, 3, 5]) → 0
 	 */
-	public int countEvens(int[] nums) {
-		return 0;
+	public int countEvens(int[] nums)
+	{
+		int countEvenNumbers = 0;
+		for (int i = 0; i < nums.length; i++) 
+		{
+			if (nums[i] % 2 == 0)
+			{
+				countEvenNumbers = countEvenNumbers +1;
+			}
+		}
+		
+		return countEvenNumbers;
 	}
 
 	/*
-	 12. Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 
-	 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do 
-	 not count.
+	 12. Return the sum of the numbers in the array, returning 0 for an empty array.
+	 Except the number 13 is very unlucky, so it does not count and numbers that come
+	 immediately after a 13 also do not count.
 	 sum13([1, 2, 2, 1]) → 6
 	 sum13([1, 1]) → 2
 	 sum13([1, 2, 2, 1, 13]) → 6
 	 sum13([1, 2, 2, 1, 13, 3]) → 6
 	 sum13([1, 2, 2, 1, 13, 3, 4]) → 10
 	 */
-	public int sum13(int[] nums) {
-		return 0;
+	public int sum13(int[] nums)
+	{
+		int sum = 0;
+		
+		for (int i = 0; i < nums.length; i++)
+		{
+			sum = sum + nums[i];
+			if (nums[i]== 13)
+			{
+				sum = sum - nums[i];
+				i = i + 1;
+			}
+		}
+		
+		return sum;
 	}
 
 	/*
-	 13. Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
+	 13. Given an array of ints, return true if the array contains a 2 next to a
+	 2 somewhere.
 	 has22([1, 2, 2]) → true
 	 has22([1, 2, 1, 2]) → false
 	 has22([2, 1, 2]) → false
 	 */
-	public boolean has22(int[] nums) {
+	public boolean has22(int[] nums)
+	{
+		for (int i = 0; i < nums.length; i++)
+		{
+			boolean hasMoreNumbers = nums.length > i + 1;
+			if (nums[i] == 2 && hasMoreNumbers && nums[i + 1] == 2)
+			{
+				return true;
+			}
+		}
 		return false;
 	}
 	
@@ -159,17 +255,40 @@ public class Exercises {
 	 lucky13([1, 2, 3]) → false
 	 lucky13([1, 2, 4]) → false
 	 */
-	public boolean lucky13(int[] nums) {
-		return false;
+	public boolean lucky13(int[] nums)
+	{
+		for (int i = 0; i < nums.length; i++)
+		{
+			if (nums[i] == 1 || nums[i] == 3)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/*
-	 15. Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
+	 15. Given an array of ints, return true if the sum of all the 2's in the array
+	 is exactly 8.
 	 sum28([2, 3, 2, 2, 4, 2]) → true
 	 sum28([2, 3, 2, 2, 4, 2, 2]) → false
 	 sum28([1, 2, 3, 4]) → false
 	 */
-	public boolean sum28(int[] nums) {
+	public boolean sum28(int[] nums)
+	{
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++)
+		{
+			if (nums[i] == 2)
+			{
+				sum = sum + nums[i];
+			}
+
+		}
+		if (sum == 8)
+		{
+			return true;
+		}
 		return false;
 	}
 
