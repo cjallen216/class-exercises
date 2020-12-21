40d921ab59a4b025ddf3ee79b2763cd8852fc88e
Chris Allen <christopherjallen216@gmail.com>
2020-12-20 19:11:51 -0500

    backing up progress

--- module-1/14_Unit_Testing/student-exercise/java/src/test/java/com/techelevator/WordCountTests.java
+++ module-1/14_Unit_Testing/student-exercise/java/src/test/java/com/techelevator/WordCountTests.java
@@ -1,5 +1,9 @@
 package com.techelevator;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,11 +21,18 @@ public class WordCountTests
 	public void howManyWords()
 	{
 		// arrange
+		String[] words = {"ba", "ba", "black", "sheep"};
 		
+		Map<String, Integer> expected = new HashMap<>();
+		expected.put("ba", 2);
+		expected.put("black", 1);
+		expected.put("sheep", 1);
 		
 		// act
+		Map<String, Integer> actual = wordCount.getCount(words);
 		
 		// assert
+		Assert.assertEquals(expected, actual);
 		
 	}
 }
