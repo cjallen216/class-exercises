256b99831ce4a249765943c584c9d97daa140af7
Chris Allen <christopherjallen216@gmail.com>
2021-01-04 21:03:33 -0500

    backup for the night

--- module-1/15_Review/Assessment/student-assessment/java/src/main/java/com/techelevator/Module1CodingAssessment.java
+++ module-1/15_Review/Assessment/student-assessment/java/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -1,31 +1,89 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Module1CodingAssessment
 {
-
 	public static void main(String[] args)
 	{
-		int year = 0;
-		String make;
-		boolean isClassicCar = false;
-		int age = 0;
-		Scanner scanner = new Scanner(System.in);
+		fileReadDemo();
+	}
+	
+	public static void workingWithFiles()
+	{
+		String filePath = "D:/data/zipcodes.txt";
+		File theFile = new File(filePath);
 		
-		// TODO Auto-generated method stub
+		System.out.println(theFile.exists());
+	}
+	
+	public static void fileReadDemo()
+	{
+		// read all zip codes in the file
+		
+		// path
+		//fileName - this is fully qualified (absolute)
+		String filePath = "D:/data/zipcodes.txt";
+		File zipCodeFile = new File(filePath);
+		
+		// open the file
+		Scanner scanner;
+		try
+		{
+			scanner = new Scanner(zipCodeFile);
+			
+			// process all lines
+			while(scanner.hasNextLine()) // when should we stop looping?
+			{
+				String line = scanner.nextLine();
+				System.out.println(line);
+				
+				
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			System.out.println("File: " + filePath + "was not found");
+		}
 		
 		
-		System.out.println("Please enter your info: ");
-		make = scanner.next();
-		year = scanner.nextInt();
+	}
+	
+	public static void flowerShowDemo()
+	{
+		FlowerShopOrder order = new FlowerShopOrder("Apology", 48);
 		
-		Car car = new Car(year, make, false);
+		System.out.println(order.toString());
 		
-		scanner.nextLine();
+		BigDecimal delivery = order.calculateDelivery(false, "29999");
+		System.out.println(delivery);
 		
-		System.out.println("Here is the data provided: " + car);
+		int numerator = 10;
+		int denominator = 0;
 		
+		if (denominator != 0);
+		{
+			int answer = numerator / denominator;
+			System.out.println(answer);
+		}
+		try
+		{
+			// some code that could cause an error
+			
+		} 
+		catch (Exception e)
+		{
+			// here's what to do if there is a problem
+			// TODO: handle exception
+		}
+		finally
+		{
+			// clean up code
+		}
 	}
 
 }
--- module-1/17_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
+++ module-1/17_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
@@ -1,10 +1,56 @@
 package com.techelevator;
 
-public class WordSearch {
+import java.io.File;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-	
-
+public class WordSearch
+{
+	public static void main(String[] args)
+	{
+		wordSearch();
+		wordsSearched();
 	}
 
+	public static void wordSearch()
+	{
+		String filePath = "alices_adventures_in_wonderland.txt";
+		File theFile = new File(filePath);
+		
+//		System.out.println(theFile.toString());
+//		System.out.println(theFile.getAbsolutePath());
+//		System.out.println(theFile.exists());
+//		System.out.println("Directory: " + theFile.isDirectory());
+//		System.out.println("File: " + theFile.isFile());
+		
+		Scanner scanner;
+		
+		try
+		{
+			scanner = new Scanner(theFile);
+			
+			while (scanner.hasNextLine())
+			{
+				String eachLine = scanner.nextLine();
+				
+			}
+			
+		} catch (Exception e)
+		{
+			// TODO: handle exception
+		}
+	}
+	
+	public static String wordsSearched()
+	{
+		Scanner wordInput = new Scanner(System.in);
+		System.out.println("What is the search word you are looking for? ");
+		String wordSearch = wordInput.nextLine();
+		
+		return wordSearch;
+	}
+	
+	public static String search()
+	{		
+		return "";
+	}
 }
