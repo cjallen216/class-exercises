494d0e0269512a3c0321b9288f6dd85f3f1f057b
Chris Allen <christopherjallen216@gmail.com>
2021-01-26 13:21:19 -0500

    backup before pulling lecture code

--- module-2/10_HTTP_Web_Services_POST/student-lecture/java/src/main/java/com/techelevator/hotels/models/Hotel.java
+++ module-2/10_HTTP_Web_Services_POST/student-lecture/java/src/main/java/com/techelevator/hotels/models/Hotel.java
@@ -1,53 +1,57 @@
 package com.techelevator.hotels.models;
 
-public class Hotel {
+public class Hotel
+{
 
-    private int id;
-    private String name;
-    private int stars;
-    private int roomsAvailable;
-    private String coverImage;
+	private int id;
+	private String name;
+	private int stars;
+	private int roomsAvailable;
+	private String coverImage;
 
-    public Hotel() {
-    }
+	public Hotel()
+	{
+	}
 
-    public Hotel(int id, String name, int stars, int roomsAvailable) {
-        this.id = id;
-        this.name = name;
-        this.stars = stars;
-        this.roomsAvailable = roomsAvailable;
-        this.coverImage = "default-cover-image.png";
-    }
+	public Hotel(int id, String name, int stars, int roomsAvailable)
+	{
+		this.id = id;
+		this.name = name;
+		this.stars = stars;
+		this.roomsAvailable = roomsAvailable;
+		this.coverImage = "default-cover-image.png";
+	}
 
-    @Override
-    public String toString() {
-        return "\n--------------------------------------------" +
-                "\n Hotel Details" +
-                "\n--------------------------------------------" +
-                "\n Id: " + id +
-                "\n Name:'" + name + '\'' +
-                "\n Stars: " + stars +
-                "\n RoomsAvailable: " + roomsAvailable +
-                "\n overImage" + coverImage;
-    }
+	@Override
+	public String toString()
+	{
+		return "\n--------------------------------------------" + "\n Hotel Details"
+				+ "\n--------------------------------------------" + "\n Id: " + id + "\n Name:'" + name + '\''
+				+ "\n Stars: " + stars + "\n RoomsAvailable: " + roomsAvailable + "\n overImage" + coverImage;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId()
+	{
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName()
+	{
+		return name;
+	}
 
-    public int getStars() {
-        return stars;
-    }
+	public int getStars()
+	{
+		return stars;
+	}
 
-    public int getRoomsAvailable() {
-        return roomsAvailable;
-    }
+	public int getRoomsAvailable()
+	{
+		return roomsAvailable;
+	}
 
-    public String getCoverImage() {
-        return coverImage;
-    }
+	public String getCoverImage()
+	{
+		return coverImage;
+	}
 }
--- module-2/10_HTTP_Web_Services_POST/student-lecture/java/src/main/java/com/techelevator/hotels/models/Reservation.java
+++ module-2/10_HTTP_Web_Services_POST/student-lecture/java/src/main/java/com/techelevator/hotels/models/Reservation.java
@@ -1,87 +1,97 @@
 package com.techelevator.hotels.models;
 
-public class Reservation {
+public class Reservation
+{
 
-    private int id;
-    private int hotelID;
-    private String fullName;
-    // Use LocalDate for dates but storing as a string to keep this example simple
-    private String checkinDate;
-    private String checkoutDate;
-    private int guests;
+	private int id;
+	private int hotelID;
+	private String fullName;
+	// Use LocalDate for dates but storing as a string to keep this example simple
+	private String checkinDate;
+	private String checkoutDate;
+	private int guests;
 
-    public Reservation() {
+	public Reservation()
+	{
 
-    }
+	}
 
-    public Reservation(int id, int hotelID, String fullName, String checkinDate, String checkoutDate, int guests) {
-        this.id = id;
-        this.hotelID = hotelID;
-        this.fullName = fullName;
-        this.checkinDate = checkinDate;
-        this.checkoutDate = checkoutDate;
-        this.guests = guests;
-    }
+	public Reservation(int id, int hotelID, String fullName, String checkinDate, String checkoutDate, int guests)
+	{
+		this.id = id;
+		this.hotelID = hotelID;
+		this.fullName = fullName;
+		this.checkinDate = checkinDate;
+		this.checkoutDate = checkoutDate;
+		this.guests = guests;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId()
+	{
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id)
+	{
+		this.id = id;
+	}
 
-    public int getHotelID() {
-        return hotelID;
-    }
+	public int getHotelID()
+	{
+		return hotelID;
+	}
 
-    public void setHotelID(int hotelID) {
-        this.hotelID = hotelID;
-    }
+	public void setHotelID(int hotelID)
+	{
+		this.hotelID = hotelID;
+	}
 
-    public String getFullName() {
-        return fullName;
-    }
+	public String getFullName()
+	{
+		return fullName;
+	}
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+	public void setFullName(String fullName)
+	{
+		this.fullName = fullName;
+	}
 
-    public String getCheckinDate() {
-        return checkinDate;
-    }
+	public String getCheckinDate()
+	{
+		return checkinDate;
+	}
 
-    public void setCheckinDate(String checkinDate) {
-        this.checkinDate = checkinDate;
-    }
+	public void setCheckinDate(String checkinDate)
+	{
+		this.checkinDate = checkinDate;
+	}
 
-    public String getCheckoutDate() {
-        return checkoutDate;
-    }
+	public String getCheckoutDate()
+	{
+		return checkoutDate;
+	}
 
-    public void setCheckoutDate(String checkoutDate) {
-        this.checkoutDate = checkoutDate;
-    }
+	public void setCheckoutDate(String checkoutDate)
+	{
+		this.checkoutDate = checkoutDate;
+	}
 
-    public int getGuests() {
-        return guests;
-    }
+	public int getGuests()
+	{
+		return guests;
+	}
 
-    public void setGuests(int guests) {
-        this.guests = guests;
-    }
+	public void setGuests(int guests)
+	{
+		this.guests = guests;
+	}
 
-
-    @Override
-    public String toString() {
-        return "\n--------------------------------------------" +
-                "\n Reservation Details" +
-                "\n--------------------------------------------" +
-                "\n Id: " + id +
-                "\n Hotel Id: " + hotelID +
-                "\n Full Name: " + fullName +
-                "\n Checkin Date: " + checkinDate +
-                "\n Checkout Date: " + checkoutDate +
-                "\n Guests: " + guests;
-    }
+	@Override
+	public String toString()
+	{
+		return "\n--------------------------------------------" + "\n Reservation Details"
+				+ "\n--------------------------------------------" + "\n Id: " + id + "\n Hotel Id: " + hotelID
+				+ "\n Full Name: " + fullName + "\n Checkin Date: " + checkinDate + "\n Checkout Date: " + checkoutDate
+				+ "\n Guests: " + guests;
+	}
 }
