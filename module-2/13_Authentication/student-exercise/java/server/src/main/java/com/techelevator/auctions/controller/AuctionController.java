62352e98b5ecd8ce970608a52377f49e4a6338ce
Chris Allen <christopherjallen216@gmail.com>
2021-02-01 17:14:30 -0500

    backup

--- module-2/13_Authentication/student-exercise/java/server/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_Authentication/student-exercise/java/server/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -4,7 +4,10 @@ import com.techelevator.auctions.DAO.AuctionDAO;
 import com.techelevator.auctions.DAO.MemoryAuctionDAO;
 import com.techelevator.auctions.exception.AuctionNotFoundException;
 import com.techelevator.auctions.model.Auction;
+import com.techelevator.auctions.security.model.User;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,52 +17,73 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
-public class AuctionController {
+@PreAuthorize("isAuthenticated()")
+public class AuctionController
+{
 
-    private AuctionDAO dao;
+	private AuctionDAO dao;
 
-    public AuctionController() {
-        this.dao = new MemoryAuctionDAO();
-    }
+	public AuctionController()
+	{
+		this.dao = new MemoryAuctionDAO();
+	}
 
-    @RequestMapping( path = "", method = RequestMethod.GET)
-    public List<Auction> list(@RequestParam(defaultValue = "") String title_like, @RequestParam(defaultValue = "0") double currentBid_lte) {
+	@PreAuthorize("permitAll")
+	@RequestMapping(path = "", method = RequestMethod.GET)
+	public List<Auction> list(@RequestParam(defaultValue = "") String title_like,
+			@RequestParam(defaultValue = "0") double currentBid_lte)
+	{
 
-        if( !title_like.equals("") ) {
-            return dao.searchByTitle(title_like);
-        }
-        if(currentBid_lte > 0) {
-            return dao.searchByPrice(currentBid_lte);
-        }
+		if (!title_like.equals(""))
+		{
+			return dao.searchByTitle(title_like);
+		}
+		if (currentBid_lte > 0)
+		{
+			return dao.searchByPrice(currentBid_lte);
+		}
 
-        return dao.list();
-    }
+		return dao.list();
+	}
 
-    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
-    public Auction get(@PathVariable int id) throws AuctionNotFoundException {
-        return dao.get(id);
-    }
+	@RequestMapping(path = "/{id}", method = RequestMethod.GET)
+	public Auction get(@PathVariable int id) throws AuctionNotFoundException
+	{
+		return dao.get(id);
+	}
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @RequestMapping( path = "", method = RequestMethod.POST)
-    public Auction create(@Valid @RequestBody Auction auction) {
-        return dao.create(auction);
-    }
+	@PreAuthorize("hasAnyRole('ADMIN', 'CREATOR')")
+	@ResponseStatus(HttpStatus.CREATED)
+	@RequestMapping(path = "", method = RequestMethod.POST)
+	public Auction create(@Valid @RequestBody Auction auction)
+	{
+		return dao.create(auction);
+	}
 
-    @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
-    public Auction update(@Valid @RequestBody Auction auction, @PathVariable int id) throws AuctionNotFoundException {
-        return dao.update(auction, id);
-    }
+	@PreAuthorize("hasAnyRole('ADMIN', 'CREATOR')")
+	@RequestMapping(path = "/{id}", method = RequestMethod.PUT)
+	public Auction update(@Valid @RequestBody Auction auction, @PathVariable int id) throws AuctionNotFoundException
+	{
+		return dao.update(auction, id);
+	}
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable int id) throws AuctionNotFoundException {
-        dao.delete(id);
-    }
+	@PreAuthorize("hasAnyRole('ADMIN', 'CREATOR')")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable int id) throws AuctionNotFoundException
+	{
+		dao.delete(id);
+	}
 
-    @RequestMapping( path = "/whoami")
-    public String whoAmI() {
-        return "";
-    }
+	@PreAuthorize("hasAnyRole('ADMIN', 'CREATOR', 'USER')")
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(path = "/whoami")
+	public String whoAmI(
+//				@Valid @RequestBody User user
+			)
+	{
+		return "user";
+//		return user.getUsername().toString();
+	}
 
 }
