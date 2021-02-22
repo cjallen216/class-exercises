ef56d7c21455b7068b6d2eb91341664db8e15ae5
Chris Allen <christopherjallen216@gmail.com>
2021-02-22 13:27:44 -0500

    backup before pulling new code

---
+++ module-3/30_Introduction_to_Vue_and_Data_Binding/student-lecture/project-demo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ProductReviews />
+  </div>
+</template>
+
+<script>
+import ProductReviews from './components/ProductReviews.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductReviews
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
