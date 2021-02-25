0c57a21c71965854d724f0db56351f512b2fda3b
Chris Allen <christopherjallen216@gmail.com>
2021-02-24 21:45:54 -0500

    backup

---
+++ module-3/32_Vue_Component_Communication/student-exercise/src/components/NewBookForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <form v-on:submit.prevent="saveBook">
+      <div class="form-element">
+          <label for="title">Title: </label>
+          <input type="text" id="title-input" />
+      </div>
+      <div class="form-element">
+          <label for="author">Author: </label>
+          <input type="text" id="author-input" />
+      </div>
+      <div class="form-element">
+          <label for="isbn">ISBN: </label>
+          <input type="text" id="isbn-input" />
+      </div>
+      <input type="submit" value="Save" />
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newBook: {
+                title: '',
+                author: '',
+                read: false,
+                isbn: ''
+            }
+        }
+    },
+    name: 'new-book-form',
+    methods: {
+        saveBook() {
+            this.newBook.read = false;
+            this.$store.commit('ADD_BOOK', this.newBook);
+            this.newBook = {
+                title: '',
+                author: '',
+                read: false,
+                isbn: ''
+            };
+        }
+    }
+}
+</script>
+
+<style>
+div.form-element {
+  margin-top: 10px;
+}
+div.form-element > label {
+  display: block;
+}
+div.form-element > input,
+div.form-element > select {
+  height: 30px;
+  width: 300px;
+}
+div.form-element > textarea {
+  height: 60px;
+  width: 300px;
+}
+form > input[type="button"] {
+  width: 100px;
+}
+form > input[type="submit"] {
+  width: 100px;
+  margin-right: 10px;
+}
+
+</style>
--- module-3/32_Vue_Component_Communication/student-exercise/src/components/ReadingList.vue
+++ module-3/32_Vue_Component_Communication/student-exercise/src/components/ReadingList.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="book-container">
-
+    <book-card v-for="book in books"
+      v-bind:key="book.card"
+      v-bind:book="book" />    
   </div>
 </template>
 
 <script>
+import BookCard from './BookCard.vue';
 
 export default {
-    name: 'reading-list'
+  components: { BookCard },
+    name: 'reading-list',
+  
+    computed: {
+    filteredBooks() {
+      const booksFilter = this.$store.state.filter;
+      const books = this.$store.state.card;
+      return books.filter(book => {
+        return booksFilter === book;
+      });
+    }
+  }
 }
 </script>
 
